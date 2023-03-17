dfce6d38924e201e3426b92b46f6f818e51d5723
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-17 11:17:52 +0100

    The order 1 is now implemented

---
+++ public/index.js
@@ -0,0 +1,74 @@
+const socket = io();
+
+let userInput = "";
+const chatMessages = document.getElementById("chat-messages");
+const sendMessage = document.getElementById("chat-form");
+let response = {};
+
+let localResponse = {};
+
+socket.on("welcome-message", (message) => {
+  displayMessage(message.welcomeList);
+  response = message;
+  localStorage.setItem("response", JSON.stringify(message));
+});
+
+socket.on("admin-message", (message) => {
+  displayMessage(message);
+//   response = message;
+});
+
+socket.on("userMessage", (message) => {
+  displayUserMessage(message.input);
+});
+
+socket.on("menu", (message) => {
+  displayMessage(message.MENU);
+});
+
+socket.on("confirmPay", (message) => {
+    // displayMessage(message.MENU);
+    // localStorage.setItem("response", JSON.stringify(message));
+    console.log(message);
+  });
+
+socket.on("saveToStorage", (message) => {
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+    localStorage.setItem("response", JSON.stringify(message));
+});
+
+sendMessage.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent submitting to a file
+  let msg = e.target.elements.msg.value; // the value of the element with id = msg
+  msg = msg.trim();
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+  localResponse = JSON.parse(localStorage.getItem("response"));
+  localResponse["input"] = msg;
+  socket.emit("chatMessage", localResponse);
+  console.log(msg);
+});
+
+function displayMessage(message) {
+  const div = document.createElement("div");
+  let list = "";
+  for (let i = 0; i < message.length; i++) {
+    list += `<li>${message[i]}</li>`;
+  }
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${"bot"} <span>${"time"}</span></p>
+  <ul id="items">
+    ${list}
+    </ul>`;
+  document.querySelector("#chat-messages").appendChild(div);
+}
+
+function displayUserMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${"bot"} <span>${"time"}</span></p>
+  <p id="item">
+    ${message}
+    </p>`;
+  document.querySelector("#chat-messages").appendChild(div);
+}
---
+++ server.js
@@ -0,0 +1,126 @@
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const dotenv = require("dotenv");
+const express = require("express");
+const session = require("express-session");
+dotenv.config({ path: "./config.env" });
+const foodList = require("./food.json");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const publicDirectoryPath = path.join(__dirname, "./public");
+
+app.use(express.static(publicDirectoryPath));
+
+const PORT = process.env.PORT || 3000;
+
+let sum = null;
+const welcomeList = [
+  "Select 1 to Place an order",
+  "Select 99 to checkout order",
+  "Select 98 to see order history",
+  "Select 97 to see current order",
+  "Select 0 to cancel order",
+];
+
+const MENU = [
+  "Bacon cheeseburger",
+  "Chicken alfredo pasta",
+  "Caesar salad",
+  "Margherita pizza",
+  "Fish and chips",
+  "Grilled salmon",
+  "New York-style cheesecake",
+  "Chocolate lava cake",
+  "Chicken wings",
+];
+
+const sessionMiddleWare = session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+});
+
+app.use(sessionMiddleWare);
+io.use((socket, next) => {
+  return sessionMiddleWare(socket.request, socket.request.res, next);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  let session = socket.id;
+  let input = "";
+  socket.emit("welcome-message", { welcomeList, session, input, MENU });
+
+  socket.on("chatMessage", (msg) => {
+    let userMealName = "";
+    let cart = [];
+    if (session === msg.session) {
+      socket.emit("userMessage", msg);
+      if (msg.input === "1" && !msg.action) {
+        msg["action"] = "subMenu";
+        socket.emit("menu", msg);
+        console.log("Select menu item");
+      } else if (msg.action === "subMenu" && !msg.checkOut) {
+        userMealName = msg.MENU[parseInt(msg.input) - 1];
+        let userMeal = foodList[userMealName];
+        // { special: [ true, 'fries', 100 ], amount: 500 }
+        let isSpecial = userMeal["special"];
+        let toSelect = [userMealName];
+        if (isSpecial[0]) {
+          toSelect.push(
+            `Select 1 to checkout without ${isSpecial[1]} - price = #${userMeal.amount}`
+          );
+          toSelect.push(
+            `Select 2 to checkout with ${isSpecial[1]} - +fee. #${
+              isSpecial[2] + userMeal.amount
+            }`
+          );
+        } else {
+          toSelect.push(
+            `Select 1 to checkout, total price - #${userMeal.amount}`
+          );
+        }
+        msg["checkOut"] = "check";
+        msg["userMealName"] = userMealName;
+        socket.emit("admin-message", toSelect);
+        console.log("Sub menu item selected");
+      } else if (msg.checkOut && msg.action) {
+        let checkOutMessage = [
+          `Cheers! ${msg.userMealName} has been put in your shopping cart...`,
+        ];
+        if (msg.input === "1") {
+          cart.push();
+        } else if (msg.input === "2") {
+        } else {
+        }
+        socket.emit("admin-message", checkOutMessage);
+        console.log("Payment option selected");
+      }
+      /*(
+            (foodChoice[1] = `Select 1 without ${special} - price = ${customerFood.amount}`)
+          )),
+            (foodChoice[2] = `Select 2 with ${special} - add. #${customerFood.special[2]}`);
+        } else {
+          foodChoice[1] = `total price - #${customerFood.amount}`;
+        }
+        if (isSpecial) {
+          sum = userMeal.amount + userMeal.special[2];
+        } else {
+          sum = userMeal.amount;
+        }
+        const paymentOption = [
+          `Total amount to pay is ${sum}`,
+          "Select 1 to Pay",
+          "Select 0 to cancel order",
+        ];*/
+    }
+    socket.emit("saveToStorage", msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
